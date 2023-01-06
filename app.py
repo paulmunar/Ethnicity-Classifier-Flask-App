296de5f89bcffcc9110a2342956ecb54652bdddb
paulmunar <paulleroymunar@gmail.com>
2023-01-06 15:06:43 -0800

    added validation to todo entry box

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, request
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from app_forms import TodoForm
 
-app  = Flask(__name__)
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
+# data - should probably be in its own file
 todos = ["Learn Flask", "Setup venv", "Build a Cool App"]
-
-class TodoForm(FlaskForm):
-    todo = StringField("Todo")
-    submit = SubmitField("Add Todo")
 
 @app.route('/', methods=["GET", "POST"])
 def index():
-    # if any request if made, and it has a "todo" attribute
-    if 'todo' in request.form:
+    todo_form = TodoForm()
+    # if any request if made, and it has a "todo" attribute (from TodoForm class)
+    if todo_form.validate_on_submit():
         # append the new todo from the form to the array
-        todos.append(request.form['todo'])
-    return render_template('index.html', todos=todos, template_form=TodoForm())
+        todos.append(todo_form.todo.data)
+    return render_template('index.html', todos=todos, template_form=todo_form)
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
