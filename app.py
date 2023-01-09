103dcc20b7d16c949122241ba24b632fc0069bcd
paulmunar <paulleroymunar@gmail.com>
2023-01-08 18:15:23 -0800

    can display race prediction and basic statistics (has to repeat creating pred_df)

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import pandas as pd
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 
+pred_df = None
+
 # data - should probably be in its own file
 todos = ["Learn Flask", "Setup venv", "Build a Cool App"]
 
@@ -25,13 +27,25 @@ def about():
 @app.route('/name_classification', methods=["GET", "POST"])
 def name_classification():
     name_form = NameForm()
-    first_name_race = None
-    last_name_race = None
+    first_name, last_name = None, None
+    first_name_race, last_name_race = None, None
     if name_form.validate_on_submit():
-        names = [{"name":name_form.first_name.data},{"name":name_form.last_name.data}]
+        first_name = name_form.first_name.data
+        last_name = name_form.last_name.data
+        names = [{"name":first_name},{"name":last_name}]
         df = pd.DataFrame(names)
         pred_df = pred_census_ln(df, "name", year=2010, num_iter=100, conf_int=0.9)
         first_name_race = pred_df["race"][0]
         last_name_race = pred_df["race"][1]
         
-    return render_template('name_classification.html', template_form=name_form, first_name_race=first_name_race, last_name_race=last_name_race)
+    return render_template('name_classification.html', template_form=name_form, first_name=first_name, last_name=last_name,
+    first_name_race=first_name_race, last_name_race=last_name_race)
+
+@app.route('/name_classification/<name>')
+def name_data(name):
+    # this is bad... repeated code
+    categories = ["api_mean", "black_mean", "hispanic_mean", "white_mean"]
+    names = [{"name":name}]
+    df = pd.DataFrame(names)
+    pred_df = pred_census_ln(df, "name", year=2010, num_iter=100, conf_int=0.9)
+    return render_template('name_data.html', name=name, pred_df=pred_df, categories=categories)
